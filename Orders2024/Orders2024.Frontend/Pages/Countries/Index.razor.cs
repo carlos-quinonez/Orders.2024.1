f113209621994f4a92879c0f4d56dffc174b01b3
Carlos Alberto Quiñonez Tellez <carlos.quinonez@outlook.com>
2024-03-30 16:25:33 -0500

    Componentes en blazor

---
+++ Orders2024/Orders2024.Frontend/Pages/Countries/Index.razor.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components;
+using Orders2024.Frontend.Repositories;
+using Orders2024.Shared.Entities;
+
+namespace Orders2024.Frontend.Pages.Countries;
+
+public partial class Index
+{
+    [Inject] IRepository Repository { get; set; } = null!;
+    public List<Country>? Countries { get; set; }
+
+    protected async override Task OnInitializedAsync()
+    {
+        await base.OnInitializedAsync();
+        var responseHttp = await Repository.GetAsyn<List<Country>>("api/countries");
+        if (!responseHttp.Error)
+        {
+            Countries = responseHttp.Response;
+        }
+    }
+}
---
+++ Orders2024/Orders2024.Frontend/_SharedComponents/GenericList.razor.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Orders2024.Frontend._SharedComponents;
+
+public partial class GenericList<Titem>
+{
+    [Parameter] public RenderFragment? Loading { get; set; }
+    [Parameter] public RenderFragment? NoRecords { get; set; }
+    [Parameter, EditorRequired] public RenderFragment Body { get; set; } = null!;
+    [Parameter, EditorRequired] public List<Titem> DataSource { get; set; } = null!;
+}
